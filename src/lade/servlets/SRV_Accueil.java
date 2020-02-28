c09fc3c4a8bdb629d805495d259de4a4ba5318b7
MokoMaracucho <desmoulin.federico@gmail.com>
2020-02-28 09:22:44 +0100

    SRV_Accueil

---
+++ src/lade/servlets/SRV_Accueil.java
@@ -0,0 +1,22 @@
+package lade.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Accueil")
+public class SRV_Accueil extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String VUE_ACCUEIL = "/WEB-INF/jsp_accueil.jsp";
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher(VUE_ACCUEIL).forward(request, response);
+	}
+}
