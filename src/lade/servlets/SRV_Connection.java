7b8ef65600cc97939914023a5e721210aaed5688
MokoMaracucho <desmoulin.federico@gmail.com>
2020-02-28 11:02:08 +0100

    SRV_Connection

---
+++ src/lade/servlets/SRV_Connection.java
@@ -0,0 +1,22 @@
+package lade.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Connection")
+public class SRV_Connection extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String VUE_CONNECTION = "/WEB-INF/jsp_connection.jsp";
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher(VUE_CONNECTION).forward(request, response);
+	}
+}
