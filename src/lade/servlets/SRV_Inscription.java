77c62d24970af4cd7ce254ba9274104312f6901c
MokoMaracucho <desmoulin.federico@gmail.com>
2020-02-28 10:23:09 +0100

    SRV_Inscription

---
+++ src/lade/servlets/SRV_Inscription.java
@@ -0,0 +1,20 @@
+package lade.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SRV_Inscription extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String VUE_INSCRIPTION = "/WEB-INF/jsp_inscription.jsp";
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher(VUE_INSCRIPTION).forward(request, response);
+	}
+}
