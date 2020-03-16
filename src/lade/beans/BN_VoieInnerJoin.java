602cf903d0efdd3879618fdc0b0632b607611662
MokoMaracucho <desmoulin.federico@gmail.com>
2020-03-16 17:36:11 +0000

    JPA : modifications erreurs sur beans

--- src/lade/beans/BN_VoieInnerJoin.java
+++ src/lade/beans/BN_VoieInnerJoin.java
@@ -1,10 +1,12 @@
 package lade.beans;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity(name="tb_voie")
 public class BN_VoieInnerJoin {
 
     @Id
